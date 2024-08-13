80b08211f4a0937af6769267afc6d2bbdb5c80e6
Alex Wohletz <awohletz3@gmail.com>
2024-08-13 15:45:58 -0600

    reorganize the project structure

--- src/pylotlight/backend/external.py
+++
@@ -1,8 +0,0 @@
-# Blob storage
-
-# S3
-
-# Some other event storage options
-
-# Event Hub
-
--- tests/test_data_ingestion.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pylotlight.data.api import ingest_data  # Assumes you have this function
-
-def test_ingest_data():
-    # Assuming ingest_data returns a dictionary of ingested data
-    result = ingest_data()
-    assert isinstance(result, dict)
-    assert "airflow" in result
-    assert "dbt" in result
